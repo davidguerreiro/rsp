584d80aff25eaff76149448df9e647a9c5e9d80b
davidguerreiro <guerrebasket@hotmail.es>
2017-03-08 15:14:20 +0000

    add hall of the fame template HTML code

--- hall-of-fame.php
+++ hall-of-fame.php
@@ -7,7 +7,28 @@ require( 'head.php' );
 
 ?>
 
-<section class="game-section">
+<section class="game-section page">
+  <h2 class="page-title">Hall of the fame</h2>
+  <p class="page-text">
+    The name of the best players in the world
+    have been recorded here for all the eternity.
+    Is your name here ?
+  </p>
+  <h3 class="page-subtitle">Players list</h3>
+  <ul class="page-list">
+    <li>
+      <ul class="page-list-sub">
+        <li>Player name</li>
+        <li>00/00/2010 12:12:12</li>
+      </ul>
+    </li>
+    <li>
+      <ul class="page-list-sub">
+        <li>Player name</li>
+        <li>00/00/2010 12:12:12</li>
+      </ul>
+    </li>
+  </ul>
 </section>
 
 
