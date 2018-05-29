86dda2ea53b8f961c464ef253061e64062423554
David <9396157+davidguerreiro@users.noreply.github.com>
2018-05-29 17:54:28 +0100

    add game init method

--- js/game/game.js
+++ js/game/game.js
@@ -15,7 +15,8 @@ var Game = {
      * @return void
      */
     init: function() {
-
+        // init players.
+        this.initPlayers();
     },
 
     /**
@@ -28,5 +29,21 @@ var Game = {
      */
     initPlayers: function() {
 
+        // create human player.
+        const data = {
+            id : 0,
+            name : 'David Guerreiro',
+        };
+        var player1 = Player( data );
+        console.log( player1 );
+
+        // create cpu player.
     },
 };
+
+document.onreadystatechange = function () {
+    if ( document.readyState == "interactive" ) {    
+        // init game.
+        game.init();
+    }
+}
